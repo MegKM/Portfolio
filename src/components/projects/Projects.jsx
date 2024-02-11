89883850241e18ed530124871a071cbca9158c1b
MegKM <megankmithen@gmail.com>
2024-02-11 12:42:05 +1100

    changed communitalk link

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -23,7 +23,7 @@ function ProjectsPage(){
             name: "CommuniTalk",
             image: "/Communitalk.png",
             description: "This is a group project built using JavaScript and the MERN stack, featuring CRUD functionality and WebSockets.",
-            link: "https://communitalk-4a7ec5f9373f.herokuapp.com/",
+            link: "https://charla-d8fb0f8c0b93.herokuapp.com/",
             githublink: "https://github.com/PyConqueror/MERN-Stack-chat-web-app"
 
         },
@@ -32,7 +32,6 @@ function ProjectsPage(){
             image: "/GrandCentral.png",
             description: "Built using Python and Django, featuring CRUD functionality for admin staff. This project is live on Heroku",
             link: "https://grandcentral-e0ebd1401088.herokuapp.com/",
-            
             githublink: "https://github.com/MegKM/grand-central"
 
         },
