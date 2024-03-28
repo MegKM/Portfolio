3a4a49380fb14d107059d17420d074a6491134f2
MegKM <megankmithen@gmail.com>
2024-03-29 10:12:17 +1100

    added jokes

--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -1,7 +1,28 @@
-import UseState from 'react'
+import { useState, useEffect } from 'react'
 import GameCard from '../gameCard/GameCard'
 
 function GamesPage(){
+    const [ joke, setJoke ] = useState("");
+    const [ punchline, setPunchline ] = useState("");
+    const [ showPunchline, setShowPunchline ] = useState(false);
+
+    useEffect(() => {
+        getJoke();
+    }, []);
+
+    async function getJoke(){
+        let request = await fetch(`https://official-joke-api.appspot.com/jokes/random`);
+        let data = await request.json();
+
+        setJoke(data.setup);
+        setPunchline(data.punchline);
+        setShowPunchline(false);
+
+    }
+
+    function revealPunchline(){
+        setShowPunchline(true);
+    }
 
     const games = [
         {
@@ -20,10 +41,26 @@ function GamesPage(){
     return (
         <>
         <div id="games" className="container pt-4">
-            <h2>Games</h2>
+            <h2>Games and such</h2>
+            <hr></hr>
+            <h4>Welcome</h4>
+            <p>This section of the site is me playing around with code and API calls. That's all.</p>
             <hr></hr>
             <div className="card-group">
                 {games.map((game, index) => <GameCard game={game} key={index} />)}
+            </div>
+            <hr></hr>
+            <div>
+                <h5>Joke town</h5>
+                <div id="joke">{ joke }</div>
+                <button className="btn btn-dark mb-2 mt-2"onClick={ revealPunchline }>
+                    <img src="/big_grin.png"></img>
+                </button>
+                {showPunchline && 
+                    <div id="punchline"> 
+                        <p>{ punchline }</p>
+                        <button className="btn btn-dark" onClick={ getJoke }>Get new joke</button>
+                    </div>}
             </div>
         </div>
         </>
